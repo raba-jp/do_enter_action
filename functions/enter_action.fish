2a1d22c9f8ef4d8c8d90cc10c803752f80df803b
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2017-06-13 11:55:09 +0900

    Fix error

--- functions/enter_action.fish
+++ functions/enter_action.fish
@@ -1,23 +1,21 @@
 function enter_action
-	[ -n $argv ]; and return 0
+	test -n $argv; and return 0
 	echo
 	echo (set_color yellow)"----- list files -----"(set_color normal)
 	ls -alG
 
 	set -l file_changed (git rev-parse --is-inside-work-tree 2> /dev/null)
-	if [ $file_changed = 'true' ]
+	if test "$file_changed" = 'true'
 		echo
 		echo (set_color yellow)"----- git status -----"(set_color normal)
 		git status --short --branch
 		echo
+
+		test -z (git config --get --local user.name 2> /dev/null); and return 0
+		test -z (git log -n 1 --oneline --author=$user_name 2> /dev/null); and return 0
+		echo (set_color yellow)"----- last commit -----"(set_color normal)
+		git log -n 1 --oneline --author=$user_name
 	end
 
-	set -l user_name (git config --get --local user.name)
-
-	[ -z $user_name ]; and return 0
-	[ -z (git log -n 1 --oneline --author=$user_name) ]; return 0
-
-	echo (set_color yellow)"----- last commit -----"(set_color normal)
-	git log -n 1 --oneline --author=$user_name
 	return 0
 end
