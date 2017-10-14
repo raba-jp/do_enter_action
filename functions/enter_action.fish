ac6662a923f2ea7c4088e7085a65cd199bd41f7a
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2017-10-14 11:58:43 +0900

    Refactoring

--- functions/enter_action.fish
+++ functions/enter_action.fish
@@ -1,26 +1,24 @@
 function enter_action
-	test -n $argv; and return 0
-	echo
-	echo (set_color yellow)"----- list files -----"(set_color normal)
-	type exa > /dev/null 2>&1
-	if test $status -eq 0
-		exa -ahlG --git --sort Name --time-style long-iso
-	else
-		ls -alG
-	end
+  function is_exist_exa
+    type exa > /dev/null 2>&1
+    echo $status
+  end
 
-	set -l file_changed (git rev-parse --is-inside-work-tree 2> /dev/null)
-	if test "$file_changed" = 'true'
-		echo
-		echo (set_color yellow)"----- git status -----"(set_color normal)
-		git status --short --branch
-		echo
+  [ -n $argv ]; and return 0
+  echo
+  echo (set_color yellow)"----- list files -----"(set_color normal)
+  [ (is_exist_exa) -eq 0 ]; and exa -ahlG --git --sort Name --time-style long-iso; or ls -alG
 
-		test -z (git config --get --local user.name 2> /dev/null); and return 0
-		test -z (git log -n 1 --oneline --author=$user_name 2> /dev/null); and return 0
-		echo (set_color yellow)"----- last commit -----"(set_color normal)
-		git log -n 1 --oneline --author=$user_name
-	end
+  if [ (git rev-parse --is-inside-work-tree 2> /dev/null) = 'true' ]
+    echo
+    echo (set_color yellow)"----- git status -----"(set_color normal)
+    git status --short --branch
+  echo
 
-	return 0
+  [ -z (git config --get --local user.name 2> /dev/null) ]; and return 0
+  [ -z (git log -n 1 --oneline --author=$user_name 2> /dev/null) ]; and return 0
+  echo (set_color yellow)"----- last commit -----"(set_color normal)
+  git log -n 1 --oneline --author=$user_name
+
+  return 0
 end
